6ec60ddfe2560831893b775ab465f5ade12c68e9
Renan Diaz <reandimo23@gmail.com>
2022-02-12 21:25:23 -0300

    Add single functions

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # google-sheets-helper
-PHP Google Spreadsheets - work with Google Sheets in a easy way
+PHP Google Spreadsheets Helper
+======================
+
+PHP Google Spreadsheets - A bunch of functions to work easily with Google Sheets API
+
+This library is a helper that encapsulate [Google APIs Client Library for PHP](https://github.com/googleapis/google-api-php-client) ([Documentation](https://developers.google.com/sheets/api/quickstart/php)) for simple usage.
+
+--- 
+
+OUTLINE
+-------
+
+* [Installation](#installation)
+* [Requirements](#requirements)
+* [Usage](#usage)
+  - [Import & Export](#import--export) 
+
+---
+
+REQUIREMENTS
+------------
+
+This library requires the following:
+
+- Dependent on [Google Client API](https://developers.google.com/sheets/api/quickstart/php)
+    - PHP 7.4 or greater with the command-line interface (CLI) and 
+    - PHP extension JSON installed
+    - A Google Cloud Platform project with the API enabled. To create a project and enable an API, refer to Create a project and enable the API
+
+---
+
+INSTALLATION
+------------
+
+Run Composer in your project:
+
+    composer require reandimo/google-sheets-helper
+    
+Then you could call it after Composer is loaded depended on your PHP framework:
+
+```php
+require __DIR__ . '/vendor/autoload.php';
+
+$credentialFilePath = 'path/to/credentials.json';
+$tokenPath = 'path/to/token.json';
+$sheet1 = new \reandimo\GoogleSheetsAPI\Helper($credentialFilePath, $tokenPath);
+```
+    
+---
+
+CREDENTIALS
+------------
+
+Google API Client needs to validate with 2 files credentials.json and token.json, the last one can be generated with a script included in the package called firstauth. You can use to generate this file for first time only to grant access to the API.
+
+Execute in your project's root folder with `php ./vendor/reandimo/google-sheets-helper/firstauth` and follow the steps.
+
+This is a 3 step script based on the quickstart.php mentioned in google's documentation (https://developers.google.com/sheets/api/quickstart/php).
+
+---
+
+USAGE
+-----
+
+### Set sheets props
+
+You can have multiple sheet instances just invoke the Helper as many times you want:
+
+```php
+$sheet1->setSpreadsheetId('somespreadsheetid');
+$sheet1->setWorksheetName('Sheet1');
+$sheet1->setSpreadsheetRange('A1:A20');
+```
--- src/apiclient/Helper.php
+++ src/apiclient/Helper.php
@@ -33,6 +33,16 @@ class Helper
     public $spreadsheetId;
 
     /**
+     * @var string the current name of the sheet in use
+     */
+    public $worksheetName;
+
+    /**
+     * @var string the current range of the sheet in use
+     */
+    public $range;
+
+    /**
      * @var string absolute path of credential file location
      */
     private $credentialFilePath;
@@ -42,15 +52,22 @@ class Helper
      */
     private $tokenPath;
 
+    /**
+     * @var string custom app name for auth in google: <https://developers.google.com/sheets/api/quickstart/php#step_2_set_up_the_sample>
+     */
+    private $appName;
+
     public function __construct(?string $credentialFilePath = null, ?string $tokenPath = null)
-    { 
+    {
         if (!file_exists($credentialFilePath)) {
             throw new Exception("No credential file in: {$credentialFilePath}");
         }
         $this->tokenPath = $tokenPath;
         $this->credentialFilePath = $credentialFilePath;
-        $this->client = $this->getClient();
-        $this->service = new \Google_Service_Sheets($this->client);
+        if (!empty($this->tokenPath) && file_exists($this->tokenPath)) {
+            $this->client = $this->getClient();
+            $this->service = new \Google_Service_Sheets($this->client);
+        }
     }
 
     public function setSpreadsheetId(?string $spreadsheetId)
@@ -68,13 +85,23 @@ class Helper
         return $this->service;
     }
 
+    public function setSpreadsheetRange(?string $range)
+    {
+        $this->range = $range;
+    }
+
+    public function getSpreadsheetRange()
+    {
+        return $this->service;
+    }
+
     public function firstAuth(?string $tokenPath = null)
     {
-
-        if( $tokenPath == null ){
+        if ($tokenPath == null) {
             throw new Exception("token.json destination filepath not set");
         }
 
+        $this->tokenPath = $tokenPath;
         $client = new \Google_Client();
         !empty($this->appName) ? $client->setApplicationName('Google Sheets API PHP') : $client->setApplicationName($this->appName);
         $client->setScopes(\Google_Service_Sheets::SPREADSHEETS);
@@ -99,12 +126,11 @@ class Helper
         mkdir(dirname($this->tokenPath), 0700, true);
         file_put_contents($this->tokenPath, json_encode($client->getAccessToken()));
 
-        if(file_exists($this->tokenPath)){
-            return true;
-        }else{
-            return false;
+        if (file_exists($this->tokenPath)) {
+            print "Token file successfully created in: {$this->tokenPath}. Congrats now you can access the API.";
+        } else {
+            throw new Exception("Token file could not be created. Try again or check your log.");
         }
-
     }
 
     /**
@@ -129,6 +155,8 @@ class Helper
         if (file_exists($this->tokenPath)) {
             $accessToken = json_decode(file_get_contents($this->tokenPath), true);
             $client->setAccessToken($accessToken);
+        } else {
+            throw new Exception("Token file does not exist in {$this->tokenPath}");
         }
 
         // If there is no previous token or it's expired.
@@ -137,38 +165,101 @@ class Helper
             if ($client->getRefreshToken()) {
                 $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
             } else {
-                // Request authorization from the user.
-                $authUrl = $client->createAuthUrl();
-                printf("Open the following link in your browser:\n%s\n", $authUrl);
-                print 'Enter verification code: ';
-                $authCode = trim(fgets(STDIN));
-
-                // Exchange authorization code for an access token.
-                $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-                $client->setAccessToken($accessToken);
-
-                // Check to see if there was an error.
-                if (array_key_exists('error', $accessToken)) {
-                    throw new Exception(join(', ', $accessToken));
-                }
+                throw new Exception("You have to run 'firstauth' in your CLI to get a new token.json");
             }
-            // Save the token to a file.
-            if (!file_exists(dirname($this->tokenPath))) {
-                mkdir(dirname($this->tokenPath), 0700, true);
-            }
-            file_put_contents($this->tokenPath, json_encode($client->getAccessToken()));
         }
         return $client;
     }
 
     /**
-     * Get Row Number Map by key or all from the actived sheet
-     * 
-     * @param string|int $key Key set by addRow()
-     * @return int|array Row number | Key-Coordinate array
+     * Inserts a single row after last row of the current spreadsheet
+     * @param string $spreadsheetId The ID of spreadsheet to insert
+     * @param string $range Range of columns for insert. Example: Sheet1!A:D 
+     * @see https://developers.google.com/sheets/api/guides/concepts
+     * @return int The number of updated rows.
      * 
      */
-    public function colorLine(?array $config)
+    public function insertSingleRow(?array $row = [])
+    {
+
+        $valueRange = new \Google_Service_Sheets_ValueRange(["values" => $row]);
+        $this->service->spreadsheets_values->append(
+            $this->getSpreadsheetId(),
+            $this->range,
+            $valueRange,
+            ["valueInputOption" => "RAW"],
+            ["insertDataOption" => "INSERT_ROWS"]
+        );
+    }
+
+    /**
+     * Update a single row after last row of the current spreadsheet
+     * @param string $spreadsheetId The ID of spreadsheet to insert
+     * @param string $range Range of columns for insert. Example: Sheet1!A:D 
+     * @see https://developers.google.com/sheets/api/guides/concepts
+     * @return int The number of updated rows.
+     * 
+     */
+    public function updateSingleRow(?array $row = []): int
+    {
+
+        if (empty($this->getSpreadsheetId()) && empty($spreadsheetId)) {
+            throw new Exception("There's no ID spreadsheet set.");
+        }
+
+        if (empty($this->getSpreadsheetRange()) && empty($this->range)) {
+            throw new Exception("There's no spreadsheet range set.");
+        }
+
+        $newCellValues = [$row];
+        $valueRange = new \Google_Service_Sheets_ValueRange(["values" => $newCellValues]);
+        $update_sheet = $this->service->spreadsheets_values->update(
+            $this->getSpreadsheetId(),
+            $this->range,
+            $valueRange,
+            ["valueInputOption" => "RAW"]
+        );
+
+        return $update_sheet->getUpdatedCells();
+    }
+    /**
+     * Quick function to update a single cell in current worksheet
+     * @param string $cell Column letter and row number of cell to update. Example: A1
+     * @param string $value New value to set. 
+     * @see https://developers.google.com/sheets/api/guides/concepts
+     * @return int The number of updated rows.
+     * 
+     */
+    public function updateSingleCell(?string $cell = null, ?string $value = null): int
+    {
+
+        if ($value == null) {
+            throw new Exception("There's no value to set.");
+        }
+
+        if ($cell == null) {
+            throw new Exception("There's no cell to set a range.");
+        }
+
+        $range = "{$this->worksheetName}!{$cell}:{$cell[0]}";
+        $valueRange = new Google_Service_Sheets_ValueRange(["values" => [$value]]);
+        $update_sheet = $service->spreadsheets_values->update($sheets::COURSEA_SHEET_ID, $range, $valueRange, ["valueInputOption" => "RAW"]);
+        return $update_sheet->getUpdatedCells();
+    }
+
+    /**
+     * Get Row Number Map by key or all from the actived sheet
+     * 
+     * @param array $config Required config to execute
+     * @param string|int $config[line] Key set by addRow()
+     * @param string|int $config[startColumnIndex] Key set by addRow()
+     * @param string|int $config[endColumnIndex] Key set by addRow()
+     * @param int        $config[r] R color code. [0,255]
+     * @param int        $config[g] G color code. [0,255]
+     * @param int        $config[b] B color code. [0,255]
+     * 
+     */
+    public function colorLine(?array $config): void
     {
 
         if (empty($config)) {
@@ -180,12 +271,12 @@ class Helper
         $config['g'] = ((int)$config['g'] / 255);
         $config['b'] = ((int)$config['b'] / 255);
 
-        $sheetId = $this->service->spreadsheets->get($this->getSpreadsheetId(), ['ranges' => $config['worksheetName']]);
+        $sheetId = $this->service->spreadsheets->get($this->getSpreadsheetId(), ['ranges' => $this->worksheetName]);
 
         $myRange = [
             'sheetId' => $sheetId->sheets[0]->properties->sheetId,
-            'startRowIndex' => $config['line'] - 1,
-            'endRowIndex' => $config['line'],
+            'startRowIndex' => $config['row'] - 1,
+            'endRowIndex' => $config['row'],
             'startColumnIndex' => $config['startColumnIndex'],
             'endColumnIndex' => $config['endColumnIndex'],
         ];
@@ -219,6 +310,5 @@ class Helper
             $batchUpdateRequest
         );
 
-        return $response;
     }
 }
